b958ff0e7d4b0eb1c041470dd5709117dc023df1
Michael Flynn <mflynn@cngann.com>
2016-09-16 10:07:25 -0700

    Adding Bootstrap Meta Data

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 ?>
 <html>
 	<head>
+		<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta http-equiv="x-ua-compatible" content="ie=edge">
+		
 		<title>Michael Flynn's Portfolio</title>
 		<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
 		<link rel="stylesheet" href="/node_modules/tether/dist/css/tether.min.css">
