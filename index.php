a3e32a1afb775e65169c8d2f0e2b6c35d7b30b0e
Michael Flynn <mflynn@cngann.com>
2016-09-16 10:05:25 -0700

    Adding tether

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
 	<head>
 		<title>Michael Flynn's Portfolio</title>
 		<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/node_modules/tether/dist/css/tether.min.css">
 	</head>
 
 	<body>
 
 		<!-- Do Javascript Last -->
 		<script src="/node_modules/jquery/dist/jquery.min.js"></script>
+		<script src="/node_modules/tether/dist/js/tether.min.js"></script>
 		<script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 		<script src="/main.js"></script>
 	</body>
