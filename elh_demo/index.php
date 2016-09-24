1051ed0bdb0c12c6b147f9708efe5c75e42c71a1
Mike Flynn <spikeon@gmail.com>
2016-09-23 23:21:15 -0700

    adding prev and next

--- elh_demo/index.php
+++ elh_demo/index.php
@@ -114,5 +114,6 @@
 		<script src="/node_modules/jquery/dist/jquery.min.js"></script>
 		<script src="/node_modules/tether/dist/js/tether.min.js"></script>
 		<script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="/node_modules/fontawesome/index.js"></script>
 	</body>
 </html>
