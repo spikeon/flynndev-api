fc8d65a6216e331ab7e894aaccabcd428b6139de
Mike Flynn <spikeon@gmail.com>
2016-09-23 22:36:33 -0700

    Making full week

--- elh_demo/index.php
+++ elh_demo/index.php
@@ -72,7 +72,7 @@
 
 					];
 					foreach($days as $day) { ?>
-						<div class='col-md-1>
+						<div class='col-md-1'>
 							<div class='list-group'>
 								<div class='list-group-item active'>
 									<div class='row'>
