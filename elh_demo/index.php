0293f808a72be67abaa2f3458bef93e8ec4250f6
Mike Flynn <spikeon@gmail.com>
2016-09-23 22:04:45 -0700

    Adding a day and a time to ELH

---
+++ elh_demo/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+	include('../vendor/autoload.php');
+?>
+<html>
+	<head lang="en">
+		<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+		<title>Michael Flynn's Portfolio</title>
+		<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/node_modules/tether/dist/css/tether.min.css">
+		<link rel="stylesheet" href="/style.min.css">
+	</head>
+
+	<body>
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-2'>
+					<div class='list-group'>
+						<div class='list-group-item active'>Sunday</div>
+						<div class='list-group-item'>12:20 <button class='btn btn-primary pull-right'> Book </button></div>
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- Do Javascript Last -->
+		<script src="/node_modules/jquery/dist/jquery.min.js"></script>
+		<script src="/node_modules/tether/dist/js/tether.min.js"></script>
+		<script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+	</body>
+</html>
