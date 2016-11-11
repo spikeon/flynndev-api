86ea3788585eac6522968beb9fc92149c2c4e7ec
Mike Flynn <spikeon@gmail.com>
2016-11-11 02:59:07 -0800

    Changing styles

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(function() {
 	$.get(apiUrl + 'experience', function(data){
 		$exp = $('.exp');
 		var smallest = 2000;
-		//for(var i in data){ if(data.year < smallest) smallest = data.year; }
+		for(var i in data){ if(data.year < smallest) smallest = data.year; }
 		var max = new Date().getFullYear() - smallest;
 		for(var i in data){
 			var exp = data[i];
