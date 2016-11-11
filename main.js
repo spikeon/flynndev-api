a65e63e3dc9c6ed7ec0ed87d20e8693211705b57
Mike Flynn <spikeon@gmail.com>
2016-11-11 02:47:20 -0800

    Changing styles

--- main.js
+++ main.js
@@ -10,20 +10,17 @@ var apiUrl = "http://flynndev.us:44562/";
 
 $(function() {
 
-    // This is where the JS for my site will live
 	$.get(apiUrl + 'experience', function(data){
 		$exp = $('.exp');
 		var smallest = 2002;
-		for(var i in data){
-			if(data.year < smallest) smallest = data.year;
-		}
+		for(var i in data){ if(data.year < smallest) smallest = data.year; }
 		var max = new Date().getFullYear() - smallest;
 		for(var i in data){
 			var exp = data[i];
 			var value = new Date().getFullYear() - exp.year;
 			$exp.append(
 				$("<div>").append(
-					$("<h4>",{text:exp.name}).append(
+					$("<h3>",{text:exp.name}).append(
 						$("<span>", {text: value + " Years", class: "float-xs-right"})
 					)
 				).append(
