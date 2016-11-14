a87d3c6b6e6447922c88eb922bb7743c72fa9c82
Mike Flynn <spikeon@gmail.com>
2016-11-13 21:46:29 -0800

    Finishing moving all api functions to the api service and out of app.controller Finishing up ensuring that all html refrences to api variables were updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,109 +3,15 @@ import { Headers, Http } 		from '@angular/http';
 
 import { PortfolioApiService } 	from './portfolio-api.service';
 
-declare var jQuery: any;
-
 
 @Component({
 	selector: 'my-app',
 	templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-
-		public token;
-		public user;
-		public error;
-		public message;
+export class AppComponent {
 
 		constructor( public api: PortfolioApiService ) {}
 
-		ngOnInit() {
-			this.loadStorage();
-		}
 
-		loadStorage() {
-			this.token = localStorage.getItem('token');
-			this.user = JSON.parse(localStorage.getItem('user'));
-			this.error = "";
-			this.message = "";
-			this.checkToken();
-		}
-
-		checkToken() {
-			if (!this.token) return;
-			this.api._get('auth', 'check')
-			.subscribe(
-				data => {
-					if (data.result) console.log("Token Still Valid");
-					else {
-						console.log("Token Invalid");
-						this.token = "";
-					}
-				},
-				err => {
-					console.log('Token Check Failed');
-					this.token = "";
-				},
-				() => {
-					console.log('Token Check Complete')
-				}
-			);
-		}
-
-		logout(event) {
-			event.preventDefault();
-			this.token = "";
-			this.message = "";
-			this.user = {};
-
-			localStorage.removeItem('token');
-			localStorage.removeItem('user');
-		}
-
-		register ( event, name, username, password ) {
-			event.preventDefault();
-			this.error = "";
-			this.message = "";
-			this.api._post( { name, username, password }, 'auth', 'register')
-				.subscribe(
-					data => {
-						jQuery('#registerModal').modal('hide');
-						jQuery('#loginModal').modal('show');
-						this.message = "Register Successfull";
-						console.log('Register Successfull')
-					},
-					err => {
-						console.log('Register Failed');
-						this.error = "Register Failed";
-					},
-					() => {
-						console.log('Register Complete');
-					}
-				);
-		}
-
-		login (event, username, password) {
-			event.preventDefault();
-			this.error = "";
-			this.api._post({username, password}, 'auth')
-				.subscribe(
-					data => {
-						this.user = data.user;
-						this.token = data.token;
-						localStorage.setItem('token', this.token);
-						localStorage.setItem('user', JSON.stringify(this.user));
-						jQuery('#loginModal').modal('hide');
-						console.log('Login Successfull')
-					},
-					err => {
-						console.log('Login Failed');
-						this.error = "Login Failed";
-					},
-					() => {
-						console.log('Login Complete')
-					}
-				);
-
-		}
 
 }
--- src/app/portfolio-api.service.ts
+++ src/app/portfolio-api.service.ts
@@ -1,12 +1,21 @@
 import { Http, Headers}	from '@angular/http';
-import { Injectable }	from '@angular/core';
+import { Injectable, OnInit }	from '@angular/core';
+
+declare var jQuery: any;
 
 @Injectable()
 export class PortfolioApiService {
-
+	public token;
+	public user;
+	public error;
+	public message;
 	public apiUrl = "http://flynndev.us:44562";
 
-	constructor ( public http: Http ) { }
+	constructor ( public http: Http ) {
+		this.loadStorage();
+	}
+
+	hasUser () { return this.token ? true : false; }
 
 	url (...segments) {
 		let url = this.apiUrl;
@@ -27,6 +36,7 @@ export class PortfolioApiService {
 	}
 
 	_post (data = {}, ...segments) {
+		console.log(this.url(...segments));
 		return this.http.post(
 			this.url(...segments),
 			JSON.stringify(data),
@@ -58,5 +68,95 @@ export class PortfolioApiService {
 	delete	( type, id ) 			{ return this._delete(type, id); }
 
 	update	( type, object ) 		{ return this._put(object, type); }
+
+
+
+
+	loadStorage() {
+		this.token = localStorage.getItem('token');
+		this.user = JSON.parse(localStorage.getItem('user'));
+		this.error = "";
+		this.message = "";
+		this.checkToken();
+	}
+
+	checkToken() {
+		if (!this.token) return;
+		this._get('auth', 'check')
+		.subscribe(
+			data => {
+				if (data.result) console.log("Token Still Valid");
+				else {
+					console.log("Token Invalid");
+					this.token = "";
+				}
+			},
+			err => {
+				console.log('Token Check Failed');
+				this.token = "";
+			},
+			() => {
+				console.log('Token Check Complete')
+			}
+		);
+	}
+
+	logout(event) {
+		event.preventDefault();
+		this.token = "";
+		this.message = "";
+		this.user = {};
+
+		localStorage.removeItem('token');
+		localStorage.removeItem('user');
+	}
+
+	register ( event, name, username, password ) {
+		event.preventDefault();
+		this.error = "";
+		this.message = "";
+		this._post( { name, username, password }, 'auth', 'register')
+			.subscribe(
+				data => {
+					jQuery('#registerModal').modal('hide');
+					jQuery('#loginModal').modal('show');
+					this.message = "Register Successfull";
+					console.log('Register Successfull')
+				},
+				err => {
+					console.log('Register Failed');
+					this.error = "Register Failed";
+				},
+				() => {
+					console.log('Register Complete');
+				}
+			);
+	}
+
+	login (event, username, password) {
+		event.preventDefault();
+		this.error = "";
+		this._post( {username, password}, 'auth' )
+			.subscribe(
+				data => {
+					this.user = data.user;
+					this.token = data.token;
+					localStorage.setItem('token', this.token);
+					localStorage.setItem('user', JSON.stringify(this.user));
+					jQuery('#loginModal').modal('hide');
+					console.log('Login Successfull')
+				},
+				err => {
+					console.log('Login Failed');
+					console.log(err);
+					this.error = "Login Failed";
+				},
+				() => {
+					console.log('Login Complete')
+				}
+			);
+
+	}
+
 
 }
