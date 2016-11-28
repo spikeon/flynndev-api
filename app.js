5ba526ae2e625580b3edb6ddda3026c21b628adc
Mike Flynn <spikeon@gmail.com>
2016-11-28 03:46:58 -0800

    Auto Deployment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var WebpackDevServer = require("webpack-dev-server");
-var config = require("./webpack.config.js");
+var WebpackDevServer    = require("webpack-dev-server");
+var webpack             = require("webpack");
+var config              = require("./webpack.config.js");
+
 //config.entry.app.unshift("webpack-dev-server/client?http://localhost:8090/");
 var compiler = webpack(config);
 var server = new WebpackDevServer(compiler);
