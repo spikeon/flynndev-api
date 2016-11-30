20ad41088650ec2c6aaaf1cc43ba34ce0c261d19
Mike Flynn <spikeon@gmail.com>
2016-11-30 01:02:22 -0800

    Changing to port 80

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 process.env.UV_THREADPOOL_SIZE = 100;
 
-
 var WebpackDevServer    = require("webpack-dev-server");
 var webpack             = require("webpack");
 var config              = require("./webpack.config.js");
 
-//config.entry.app.unshift("webpack-dev-server/client?http://localhost:8090/");
+var compiler            = webpack(config);
 
-var compiler = webpack(config);
+var server              = new WebpackDevServer(compiler);
 
-var server = new WebpackDevServer(compiler);
-
-server.listen(8090);
+server.listen(80);
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path:             helpers.root('dist'),
-    publicPath:       'http://localhost:8090/',
+    publicPath:       'http://localhost/',
     filename:         '[name].js',
     chunkFilename:    '[id].chunk.js'
   },
