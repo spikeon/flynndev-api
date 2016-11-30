a012f48a54e8db5026375f9e70ca8245fe8f906d
Mike Flynn <spikeon@gmail.com>
2016-11-30 02:01:38 -0800

    Auto Deployment

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path:             helpers.root('dist'),
-    publicPath:       'http://flynndev.us/',
+    publicPath:       '/',
     filename:         '[name].js',
     chunkFilename:    '[id].chunk.js'
   },
