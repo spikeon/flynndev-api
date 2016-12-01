c4c9133ff991a83dfb218a7a4e6b6459b4c2198b
Mike Flynn <spikeon@gmail.com>
2016-12-01 02:14:19 -0800

    Auto Deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ process.env.UV_THREADPOOL_SIZE = 100;
 var WebpackDevServer    = require("webpack-dev-server");
 var webpack             = require("webpack");
 var config              = require("./webpack.config.js");
-
+var express             = require("express");
 var compiler            = webpack(config);
 
 var server              = new WebpackDevServer(compiler, {
