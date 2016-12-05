49e84a83afd20690bb4f91381a3ddf409d92c090
Mike Flynn <spikeon@gmail.com>
2016-12-05 05:22:46 -0800

    Auto Deployment

--- frontend_server.js
+++ frontend_server.js
@@ -9,4 +9,4 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/projects',    express.static('/var/projects'));
 app.use('/api',         express.static(path.join(__dirname , 'api')));
 app.all('/*', function(req, res) { res.sendFile(path.join(__dirname, 'dist', 'index.html')); });
-app.listen(80);
+app.listen(17580);
