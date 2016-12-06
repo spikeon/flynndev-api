984e90c820630f302fca77075f581b62d1713470
Mike Flynn <spikeon@gmail.com>
2016-12-05 18:56:48 -0800

    Auto Deployment

--- src/app/project/featured-projects.component.ts
+++ src/app/project/featured-projects.component.ts
@@ -1,16 +1,11 @@
 import { Component } 	from '@angular/core';
-
 import { PortfolioApiService }	from '../portfolio-api.service';
-
 import { LoggerService }        from '../logger.service';
-
-import {ProjectsService}        from "./projects.service";
-
-declare var jQuery: any;
+import { ProjectsService }        from "./projects.service";
 
 @Component({
 	selector: 'featured-projects',
-	templateUrl: './projects.component.html'
+	templateUrl: './featured-projects.component.html'
 })
 export class FeaturedProjectsComponent {
 
