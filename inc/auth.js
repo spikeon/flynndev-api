729f5f0c73e4ee9284389ff3164818d1a996e0b0
Mike Flynn <spikeon@gmail.com>
2016-12-05 19:04:52 -0800

    Auto Deployment

--- inc/auth.js
+++ inc/auth.js
@@ -2,22 +2,25 @@ var express		= require('express');
 var jwt			= require('jsonwebtoken');
 var config		= require('../config/api_config');
 
+let debug = true;
+
 module.exports 	= {
 
-	Init : function(req,res,next){
+	Init : function( req, res, next){
+
 		var token = req.headers['x-access-token'];
 
 		if(token) jwt.verify(token, config.secret, (err, decoded) => {
 			if(err) {
-				console.log(err);
-				console.log("Token Errored");
+				if(debug) console.log(err);
+				if(debug) console.log("Token Errored");
 			}
-			console.log("Token Decoded");
-			console.log(decoded);
+			if(debug) console.log("Token Decoded");
+			if(debug) console.log(decoded);
 			req.decoded = err ? false : decoded;
 		});
 		else {
-			console.log("No Token Sent");
+			if(debug) console.log("No Token Sent");
 			req.decoded = false;
 		}
 
@@ -25,8 +28,9 @@ module.exports 	= {
 	},
 
 	Token : function(req,res,next){
+		if(debug) console.log("Checking Token");
 		if(req.decoded !== false){
-			//console.log("Auth Passed");
+			if(debug) console.log("Auth Passed");
 			next();
 		} 
 		else return res.status(403).send({success: false, message: 'No Token Provided'});
@@ -34,6 +38,8 @@ module.exports 	= {
 	},
 
 	Admin : function(req, res, next){
+		if(debug) console.log("Checking Admin");
+		if(debug) console.log(req.decoded);
 		if(req.decoded !== false && req.decoded.admin === 1) next();
 		else res.sendStatus(403);
 	}
