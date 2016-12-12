1b284412a7a9548b8d96540be2bddaadbf3c3714
mike flynn <spikeon@gmail.com>
2016-12-12 01:11:38 -0800

    Auto Deployment

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,6 +4,8 @@ import { PortfolioApiService }	from '../portfolio-api.service';
 
 import { LoggerService }        from '../logger.service';
 
+import { Mail }                 from './mail';
+
 declare var jQuery: any;
 
 @Component({
@@ -12,14 +14,44 @@ declare var jQuery: any;
 })
 export class ContactComponent implements OnInit {
 
+	public submitted:boolean = false;
+
+	model = new Mail("","","");
+
 	constructor( public api: PortfolioApiService, public log: LoggerService ) { }
 
 	ngOnInit(): void {
 
 	}
 
+	newMessage(): void {
+		this.model = new Mail("","","");
+	}
+
 	sendTestMessage(): void {
 		this.api.sendEmail("Test Dude", "test@test.com", "This is but a test of the test system.  \n Is this on a new line?")
+	}
+
+	sendMessage(): void {
+
+		let email_re = /\S+@\S+\.\S+/;
+
+		if( ! this.model.name ) {
+			// Name is blank
+
+		}
+		else if( ! email_re.test(this.model.email) ) {
+			// Email is invalid
+
+		}
+		else if( ! this.model.content ){
+			// Content is blank
+
+		}
+		else {
+			this.api.sendEmail( this.model.name , this.model.email , this.model.content );
+		}
+
 	}
 
 }
---
+++ src/app/contact/mail.ts
@@ -0,0 +1,7 @@
+export class Mail {
+	constructor(
+		public name: string,
+		public email: string,
+		public content: string
+	) {  }
+}
