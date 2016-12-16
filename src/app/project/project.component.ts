0cda66201bff13c72c1df3999306c32ad6aea7e9
mike flynn <spikeon@gmail.com>
2016-12-15 18:57:50 -0800

    Auto Deployment

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -30,7 +30,7 @@ export class ProjectComponent implements OnInit, AfterViewInit {
 	ngOnInit() { }
 
 	ngAfterViewInit(){
-		this.changeFile(this.projects.currentProject.files.length > 0 ? this.projects.currentProject.files[0] : { name: '404', content : " /* Sorry, this project doesn't have any files currently */ " });
+		if(this.api.hasUser()) this.changeFile(this.projects.currentProject.files.length > 0 ? this.projects.currentProject.files[0] : { name: '404', content : " /* Sorry, this project doesn't have any files currently */ " });
 		this.changeImage(this.projects.currentProject.gallery[0]);
 	}
 
